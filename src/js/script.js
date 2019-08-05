c2c21502b48e06e8ccdc0eba2ab1e9f0e1bc5d1e
Dias Nurdikanov <dison08@mail.ru>
2019-08-05 23:56:34 +0600

    добавляет сборку

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// подключение js-файлов с помощью rigger-а
+
+
+// библиотеки/полифилы и т.д.
+
+//= third-party/jquery-3.3.1.min.js
+//= third-party/ofi.js
+
+// собственные файлы
+
+
+// IE object-fit polyfill
+objectFitImages();
